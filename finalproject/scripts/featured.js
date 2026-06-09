fe22d97305f50c2afbb52f8a2bb3577ffcaefcfa
mfullerton4 <mfullerton4@gmail.com>
2026-06-09 07:26:50 -0600

    i made changes

--- finalproject/scripts/featured.js
+++ finalproject/scripts/featured.js
@@ -1,18 +1,18 @@
-import { trails } from "./trails.js";
+import {getTrails } from "./trailData.js";
 
 const featuredContainer = document.querySelector("#featured-container");
 
 async function loadFeatured() {
-    const trails = await getTrails();
+    const trails = await getTrails();   // FIXED
 
     const featured = trails.filter(trail => trail.featured === true);
 
     featured.forEach(trail => {
         const card = document.createElement("div");
-        card.classList.add("trail-card");
+        card.classList.add("featured-card");
 
         card.innerHTML = `
-        <img src="${trail.image}" alt= "${trail.name}">
+        <img src="${trail.image}" alt="${trail.name}">
         <h3>${trail.name}</h3>
         <span class="badge ${trail.difficulty.toLowerCase()}">${trail.difficulty}</span>
         <p>${trail.distance_miles} miles • ${trail.elevation_gain_ft} ft gain</p>
